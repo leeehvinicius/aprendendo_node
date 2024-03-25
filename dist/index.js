34c0b935344beb18eb387cc3ae250f5c02075bca
LEANDRO VINICIUS FEIJO MELO <122054743+leeehvinicius@users.noreply.github.com>
2024-03-25 14:27:44 -0400

    aprendendo a somar em node

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const matematica = require("./matematica");
+let n1 = 10;
+let n2 = 2;
+console.log(`soma = ${matematica.somar(n1, n2)}`);
+console.log(`subtraçao = ${matematica.subtrair(n1, n2)}`);
+console.log(`multiplicao = ${matematica.multiplicar(n1, n2)}`);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const matematica = require("./matematica")
+let n1:number=10;
+let n2:number=2;
+
+
+console.log(`soma = ${matematica.somar(n1,n2)}`);
+console.log(`subtraçao = ${matematica.subtrair(n1,n2)}`)
+console.log(`multiplicao = ${matematica.multiplicar(n1,n2)}`)
