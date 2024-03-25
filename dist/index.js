01879799f7b819041b0c359873140640df008dd2
LEANDRO VINICIUS FEIJO MELO <122054743+leeehvinicius@users.noreply.github.com>
2024-03-25 15:35:55 -0400

    instalando dependencias

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // const matematica = require("./matematica")
 const matematica_1 = require("./matematica");
+const validator_1 = __importDefault(require("validator"));
 let n1 = 10;
 let n2 = 2;
 console.log(`soma = ${(0, matematica_1.somar)(n1, n2)}`);
 console.log(`subtraçao = ${(0, matematica_1.subtrair)(n1, n2)}`);
 console.log(`multiplicao = ${(0, matematica_1.multiplicar)(n1, n2)}`);
+console.log(validator_1.default.isEmail('suporte@klrentacar.com.br'));
+console.log(validator_1.default.isIP('192.168.0.1'));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // const matematica = require("./matematica")
 import {somar,subtrair,multiplicar} from './matematica';
+import validator from 'validator';
 let n1:number=10;
 let n2:number=2;
 
@@ -7,3 +8,8 @@ let n2:number=2;
 console.log(`soma = ${somar(n1,n2)}`);
 console.log(`subtraçao = ${subtrair(n1,n2)}`)
 console.log(`multiplicao = ${multiplicar(n1,n2)}`)
+
+
+console.log(validator.isEmail('suporte@klrentacar.com.br'))
+
+console.log(validator.isIP('192.168.0.1'))
